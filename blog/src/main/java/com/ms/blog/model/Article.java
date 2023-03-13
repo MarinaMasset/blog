43581dc91c2960414b11274f1187bbdbf7216d5a
SiderealStorm <darkwolf_59@live.fr>
2023-03-13 15:02:38 +0100

    Started backend all over because wasn't compiling ; now identical to remote main, working

--- blog/src/main/java/com/ms/blog/model/Article.java
+++ blog/src/main/java/com/ms/blog/model/Article.java
@@ -1,36 +1,33 @@
 package com.ms.blog.model;
 
-import java.sql.Date;
+import java.util.Date;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
-	@Entity
-	@Table(name = "articles")
-	public class Article {
+@Entity
+@Table(name = "articles")
+public class Article {
 
-		@Id
-		@GeneratedValue(strategy = GenerationType.AUTO)
-		private int id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
 
-		@Column(name = "title")
-		private String title;
+	@Column(name = "title")
+	private String title;
 
-		@Column(name = "content")
-		private String content;
+	@Column(name = "content")
+	private String content;
 
-		@Column(name = "date")
-		private Date date;
+	@Column(name = "date")
+	private Date date;
 
-		@Column(name = "type")
-		private String type;
-		
-		@Column(name = "author")
-		private String author;
-		
-		@Column(name = "picture")
-		private String picture;
-		
-		public Article() {
-
-		}
+	@Column(name = "type")
+	private String type;
+	
+	@Column(name = "author")
+	private String author;
+	
+	@Column(name = "picture")
+	private String picture;
+	
 }
--- blog/src/main/java/com/ms/blog/repository/ArticleRepository.java
+++ blog/src/main/java/com/ms/blog/repository/ArticleRepository.java
@@ -1,10 +1,11 @@
 package com.ms.blog.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ms.blog.model.Article;
 
+@Repository
 public interface ArticleRepository extends JpaRepository<Article, Integer> {
-	
-	
+
 }
